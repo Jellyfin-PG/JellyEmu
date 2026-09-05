97063f85a40e3ed0dfe03512cb57331785544595
Rob Loach <robloach@gmail.com>
2026-09-05 17:25:16 -0400

    Use shared browser UA in GogProvider

--- Providers/GogProvider.cs
+++ Providers/GogProvider.cs
@@ -28,7 +28,7 @@ namespace JellyEmu.Providers
         protected HttpClient GetHttpClient()
         {
             var client = HttpClientFactory.CreateClient();
-            client.DefaultRequestHeaders.Add("User-Agent", $"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 {JellyEmuVersion.UserAgent}");
+            client.DefaultRequestHeaders.Add("User-Agent", JellyEmuVersion.BrowserUserAgent);
             return client;
         }
 
